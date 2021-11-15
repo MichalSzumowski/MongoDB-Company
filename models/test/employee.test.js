31b42e490e03bc851e0c964f2940eb9ac4b65e39
Michał Szumowski <mic.szumowski@gmail.com>
2021-11-15 19:46:21 +0100

    Add tests for department and employee models - ex 30.2

---
+++ models/test/employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require('../employee.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+after(() => {
+  mongoose.models = {};
+});
+
+describe('Employee', () => {
+  it('should throw an error if no "firstName", "lastName", "department" arg', () => {
+    const empl = new Employee({});
+    empl.validate(err => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+  });
+
+  it('should throw an error if "firstName", "lastName", "department" are not a string', () => {
+    const empl = new Employee({ firstName: {}, lastName: [], department: undefined });
+    empl.validate(err => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+  });
+
+  it('should not throw an error if "firstName", "lastName", "department" are okay', () => {
+    const empl = new Employee({ firstName: 'Denis', lastName: 'Rodman', department: 'Management' });
+    empl.validate(err => {
+      expect(err).to.not.exist;
+    });
+  });
+}); 
